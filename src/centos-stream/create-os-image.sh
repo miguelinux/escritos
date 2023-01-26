b855253d11e0e151fdeeb3cfa0c011f7593ee4bf
Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
2023-01-26 01:39:51 -0600

    coi: sync with qs

--- src/centos-stream/create-os-image.sh
+++ src/centos-stream/create-os-image.sh
@@ -6,12 +6,13 @@
 
 ##################  start default config  ##################
 VM_NAME=vm001
-VM_DEV_NET="virtio-net-pci"                               # VM type NIC 
-VM_MONITOR="" #${HOME}/.cache/qemu/${VM_NAME}-vm-monitor.sock # Monitor unix socket
+VM_DEV_NET="virtio-net-pci"                               # VM type NIC
+VM_MONITOR="" #"${HOME}/.cache/qemu/${VM_NAME}-vm-monitor.sock" # Monitor unix socket
 VM_MEM=2048                                               # VM RAM
 VM_CPU=host                                               # CPU type
 VM_SMP="cpus=4,cores=2,threads=2,sockets=1"               # SMP
-VM_SERIAL=none                                            # VM Serial 
+VM_SERIAL=none                                            # VM Serial
+VM_RTC_WIN="" #"-rtc base=localtime"  # if OS is windows set to localtime
 VM_IMG_1=""                                               # VM Image 1
 VM_IMG_FMT_1=qcow2       # raw|qcow2|luks|vmdk|vpc|VHDX
 VM_IMG_CACHE_1=writeback # writethrough|writeback(Default)|none|directsync|unsafe
@@ -26,10 +27,13 @@ VM_BOOT_ISO=1                                             # VM ISO 1 bootindex
 VM_NETDEV=user                                            # user|tap
 VM_TAP=""                                                 # Network tap device name
 VM_BRIDGE=""                                              # Network bridge name
+VM_NET_USER_HOSTFWD=",hostfwd=tcp::10022-:22"             # Host forwarding
 VM_NETDEV_EXTRA=""                                        # Extra config filled in my_setup
 VM_MACADDRESS=52:54:00:a1:b2:c3                           #
 VM_OVMF_CODE=${HOME}/.local/share/qemu/OVMF_CODE.fd
 VM_OVMF_VARS=${HOME}/.local/share/qemu/OVMF_VARS-${VM_NAME}.fd
+VM_SPICE_PORT=5924                                        # spice port
+VM_SPICE_EXTRA=",disable-ticketing=on"                    # spice extra args
 VM_KERNEL=""
 VM_INITRD=""
 VM_KCMDLINE=""
@@ -43,6 +47,7 @@ EXTRA_QEMU_ARGS=""       # -hdd fat:/my_directory
 
 # Where is the qemu binary
 QEMU_BIN=$(command -v qemu-system-x86_64)
+QEMU_DAEMONIZE="" #"-daemonize"
 KERNEL_DIR=""
 
 die ()
@@ -114,6 +119,10 @@ my_setup ()
         then
             mkdir -p ${VM_OVMF_CODE%/*}
             cp /usr/share/edk2/ovmf/OVMF_CODE.cc.fd ${VM_OVMF_CODE}
+        elif [ -f /usr/share/OVMF/OVMF_CODE.fd ]
+        then
+            mkdir -p ${VM_OVMF_CODE%/*}
+            cp /usr/share/OVMF/OVMF_CODE.fd ${VM_OVMF_CODE}
         else
             die "Can not find OVMF_CODE.fd file"
         fi
@@ -129,6 +138,14 @@ my_setup ()
         then
             mkdir -p ${VM_OVMF_VARS%/*}
             cp /usr/share/edk2/ovmf/OVMF_VARS.fd ${VM_OVMF_VARS}
+        elif [ -f /usr/share/edk2/ovmf/OVMF_VARS.secboot.fd ]
+        then
+            mkdir -p ${VM_OVMF_VARS%/*}
+            cp /usr/share/edk2/ovmf/OVMF_VARS.secboot.fd ${VM_OVMF_VARS}
+        elif [ -f /usr/share/OVMF/OVMF_VARS.fd ]
+        then
+            mkdir -p ${VM_OVMF_VARS%/*}
+            cp /usr/share/OVMF/OVMF_VARS.fd ${VM_OVMF_VARS}
         else
             die "Can not find OVMF_VARS.fd file"
         fi
@@ -142,7 +159,10 @@ my_setup ()
     else
         # Ensure we use user netdev
         VM_NETDEV=user
-        VM_NETDEV_EXTRA=""
+        if test -n "${VM_NET_USER_HOSTFWD}"
+        then
+            VM_NETDEV_EXTRA="${VM_NETDEV_EXTRA}${VM_NET_USER_HOSTFWD}"
+        fi
     fi
 
     # Create cache dir if not exist
@@ -160,6 +180,11 @@ my_setup ()
         then
             QEMU_BIN=/usr/libexec/qemu-kvm
         fi
+    fi
+
+    if [ "${VM_SERIAL}" = "stdio" ]
+    then
+        QEMU_DAEMONIZE=""
     fi
 }
 
@@ -371,13 +396,14 @@ extract_kernel ()
 run_qemu ()
 {
     ${QEMU_BIN}                                                         \
+        ${QEMU_DAEMONIZE}                                               \
         -name ${VM_NAME}                                                \
         -cpu  ${VM_CPU}                                                 \
         -machine type=q35,accel=kvm,usb=on                              \
         -enable-kvm                                                     \
         -smp ${VM_SMP}                                                  \
         -m ${VM_MEM}                                                    \
-        -rtc base=localtime                                             \
+        ${VM_RTC_WIN}                                                   \
         -monitor unix:${VM_MONITOR},server,nowait                       \
         -serial ${VM_SERIAL}                                            \
         -parallel none                                                  \
@@ -389,9 +415,9 @@ run_qemu ()
         -device virtio-blk-pci,drive=disk0,iothread=io1,bootindex=${VM_BOOT_IMG} \
         -drive file=${VM_OVMF_CODE},if=pflash,format=raw,unit=0,readonly=on \
         -drive file=${VM_OVMF_VARS},if=pflash,format=raw,unit=1         \
-        -drive file=${VM_IMG_1},format=${VM_IMG_FMT_1},cache=${VM_IMG_CACHE_1},if=none,index=0,aio=native,id=disk0 \
+        -drive file=${VM_IMG_1},format=${VM_IMG_FMT_1},cache=${VM_IMG_CACHE_1},if=none,index=0,aio=native,cache.direct=on,id=disk0 \
         ${VM_EXTRA_BLOCKS}                                              \
-        -spice port=5924,disable-ticketing=on                           \
+        -spice port=${VM_SPICE_PORT}${VM_SPICE_EXTRA}                   \
         -device qxl-vga                                                 \
         -device virtio-serial                                           \
         -netdev ${VM_NETDEV},id=mynet0${VM_NETDEV_EXTRA}                \
