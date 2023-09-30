842bf4fe2b55a33ba233f1c725dc1eb1ca3f572c
Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
2023-09-30 12:13:15 -0600

    debian: setup kernel dev env

---
+++ src/debian/setup-kernel-dev-env.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
+# ex: ts=8 sw=4 sts=4 et filetype=sh
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+source /usr/lib/os-release
+
+if [ $UID -ne 0 ]
+then
+    echo "Please run with \"root\" or \"sudo $0\""
+    exit 1
+fi
+
+if [ "$NAME" = "Debian GNU/Linux" ]
+then
+    if [ -d /etc/apt/sources.list.d ]
+    then
+        apt-get update
+        apt-get install git stow tmux vim rsync connect-proxy curl xmlto \
+                        build-essential fakeroot devscripts libncurses-dev
+        apt-get build-dep linux
+    fi
+fi
