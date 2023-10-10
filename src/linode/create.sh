66f3b8409dae4d1b9ad84aae4be57f1212d5562b
Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
2023-10-09 20:15:01 -0600

    linode: add ubuntu

--- src/linode/create.sh
+++ src/linode/create.sh
@@ -3,11 +3,17 @@
 # ex: ts=8 sw=4 sts=4 et filetype=sh
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
+#
+#L_LABEL=Debian12
+#L_IMAGE=linode/debian12 
+
+L_LABEL=Ubuntu2204LTS
+L_IMAGE=linode/ubuntu22.04
 
 linode-cli linodes create \
   --type   g6-nanode-1 \
   --region us-west \
-  --label  Debian12 \
-  --image  linode/debian12 \
+  --label  ${L_LABEL} \
+  --image  ${L_IMAGE} \
   --root_pass "$(< $HOME/.ssh/linode.root.passwd)" \
   --authorized_keys "$(< $HOME/.ssh/linode.key.pub)"
