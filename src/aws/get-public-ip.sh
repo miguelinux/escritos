6cf058d373f91876c43f8a7c552ae771c7561e72
Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
2023-10-24 19:58:16 -0600

    add aws basic commands

---
+++ src/aws/get-public-ip.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
+# ex: ts=8 sw=4 sts=4 et filetype=sh
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# --instance-ids i-XXX
+VM_ID=""
+
+aws ec2 describe-instances ${VM_ID} --query 'Reservations[*].Instances[*].[InstanceId, PublicIpAddress]'
