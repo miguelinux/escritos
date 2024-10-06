2907dfae6c83fc1800acb1863d450ebbf42f02f2
Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
2024-10-06 06:38:05 -0600

    salsa: iniciando setup kernel repo

---
+++ src/debian/salsa/setup-kernel-repo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
+# ex: ts=8 sw=4 sts=4 et filetype=sh
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+
+if [ ! -f debian/rules ]
+then
+    echo "No debian/rules found"
+    exit 1
+fi
+
