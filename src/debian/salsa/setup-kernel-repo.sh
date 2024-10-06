1d9c1bf2159bb43aa9bc3202af644afcc5442451
Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
2024-10-06 08:10:22 -0600

    casi terminado el setup kernel repo

--- src/debian/salsa/setup-kernel-repo.sh
+++ src/debian/salsa/setup-kernel-repo.sh
@@ -4,6 +4,7 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+set -e
 
 if [ ! -f debian/rules ]
 then
@@ -11,3 +12,31 @@ then
     exit 1
 fi
 
+deb_pkg=$(dpkg-parsechangelog -S Source)
+
+if [ "$deb_pkg" != "linux" ]
+then
+    echo "No linux debian source"
+    exit 2
+fi
+
+ARCH=$(dpkg --print-architecture)
+FEATURESET=none
+# User ARCH on amd64
+FLAVOUR=$ARCH
+
+# debian/rules maintainerclean
+
+deb_kernel_ver=$(dpkg-parsechangelog -S Version)
+kernel_ver=${deb_kernel_ver%-*}
+kernel_file=linux-${kernel_ver}.tar.xz
+deb_kernel_file=linux_${kernel_ver}.orig.tar.xz
+
+if [ ! -f ../${deb_kernel_file} ]
+then
+    curl -L -o ../${deb_kernel_file} https://cdn.kernel.org/pub/linux/kernel/v6.x/${kernel_file}
+fi
+
+debian/rules orig
+debian/rules debian/control
+dpkg-architecture -a$ARCH -c make -f debian/rules.gen setup_${ARCH}_${FEATURESET}_${FLAVOUR}
