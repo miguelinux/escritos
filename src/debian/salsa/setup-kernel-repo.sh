dbf26cc4523100a047f079039dbc4f881e1082de
Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
2024-10-23 21:06:46 -0600

    debian: salsa: add maintainerclean

--- src/debian/salsa/setup-kernel-repo.sh
+++ src/debian/salsa/setup-kernel-repo.sh
@@ -25,10 +25,29 @@ FEATURESET=none
 # User ARCH on amd64
 FLAVOUR=$ARCH
 
-# debian/rules maintainerclean
-
 deb_kernel_ver=$(dpkg-parsechangelog -S Version)
 kernel_ver=${deb_kernel_ver%-*}
+
+if [ -f Makefile ]
+then
+    kv=$(grep ^VERSION      Makefile | cut -f 3 -d \ )
+    kp=$(grep ^PATCHLEVEL   Makefile | cut -f 3 -d \ )
+    ks=$(grep ^SUBLEVEL     Makefile | cut -f 3 -d \ )
+    ke=$(grep ^EXTRAVERSION Makefile | cut -f 3 -d \ )
+
+    # Local Kernel Version
+    lkv=$kv.$kp.$ks
+    if [ -n "$ke" ]
+    then
+        lkv=$kv.$kp$ke
+    fi
+
+    if [ "$kernel_ver" != "$lkv" ]
+    then
+        debian/rules maintainerclean
+    fi
+fi
+
 kernel_file=linux-${kernel_ver}.tar.xz
 deb_kernel_file=linux_${kernel_ver}.orig.tar.xz
 
