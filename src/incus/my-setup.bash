e6795c46e6f4ad87c7a7ed7e1a96194678fb2204
Miguel Bernal Marin <miguel.bernal.marin@gmail.com>
2025-09-15 12:49:37 -0600

    incus: set my_manager variable
    Now you can use LXD/LCX on Ubuntu with this script

--- src/incus/my-setup.bash
+++ src/incus/my-setup.bash
@@ -4,6 +4,9 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+
+my_manager=incus
+
 setup_my_user ()
 {
         user_id=$1
@@ -14,14 +17,14 @@ setup_my_user ()
         then
             proxy_env="--env=https_proxy=$https_proxy"
         fi
-        incus exec ${contenedor} --user ${user_id} --group ${user_id} -- \
+        $my_manager exec ${contenedor} --user ${user_id} --group ${user_id} -- \
             mkdir -m 700 -p ${user_home}/.ssh
-        incus file push incus.key.pub \
+        $my_manager file push incus.key.pub \
             ${contenedor}${user_home}/.ssh/authorized_keys --uid ${user_id} --gid ${user_id}
 
-        incus exec ${contenedor} --user ${user_id} --group ${user_id} -- \
+        $my_manager exec ${contenedor} --user ${user_id} --group ${user_id} -- \
             mkdir -p ${user_home}/git
-        incus exec ${contenedor} --user ${user_id} --group ${user_id} $proxy_env -- \
+        $my_manager exec ${contenedor} --user ${user_id} --group ${user_id} $proxy_env -- \
             git -C ${user_home}/git clone https://github.com/miguelinux/dotfiles.git
 
         if [ -n "$http_proxy" ]
@@ -35,13 +38,26 @@ setup_my_user ()
             echo export HTTPS_PROXY=$https_proxy >> $proxyfile
             echo export NO_PROXY=$no_proxy       >> $proxyfile
 
-            incus exec ${contenedor} --user ${user_id} --group ${user_id} -- \
+            $my_manager exec ${contenedor} --user ${user_id} --group ${user_id} -- \
                 mkdir -p ${user_home}/.config/shrc
-            incus file push $proxyfile \
+            $my_manager file push $proxyfile \
                 ${contenedor}${user_home}/.config/shrc/proxy --uid ${user_id} --gid ${user_id}
             rm $proxyfile
         fi
 }
+
+###############################################################################
+
+source /usr/lib/os-release
+
+case $ID in
+    debian)
+        my_manager=incus
+    ;;
+    ubuntu)
+        my_manager=lxc
+    ;;
+esac
 
 contenedor=$1
 
@@ -51,13 +67,13 @@ then
     exit 1
 fi
 
-if !  incus ls ${contenedor} | grep --quiet RUNNING
+if !  $my_manager ls ${contenedor} | grep --quiet RUNNING
 then
     >&2 echo "No se encontro el contenedor: ${contenedor} corriendo"
     exit 2
 fi
 
-source <(incus exec ${contenedor} cat /etc/os-release)
+source <($my_manager exec ${contenedor} cat /etc/os-release)
 
 packages_to_install="git git-lfs tmux vim rsync curl"
 packages_to_install_too="stow connect-proxy"
@@ -73,37 +89,37 @@ case $ID in
         if [ -n "$http_proxy" ]
         then
             dnf_conf=$(mktemp)
-            incus file pull ${contenedor}/etc/dnf/dnf.conf $dnf_conf
+            $my_manager file pull ${contenedor}/etc/dnf/dnf.conf $dnf_conf
             echo "proxy=$http_proxy" >> $dnf_conf
-            incus file push $dnf_conf ${contenedor}/etc/dnf/dnf.conf
+            $my_manager file push $dnf_conf ${contenedor}/etc/dnf/dnf.conf
             rm $dnf_conf
         fi
-        incus exec ${contenedor} -- dnf -y update
-        incus exec ${contenedor} -- dnf -y install $packages_to_install
-        incus exec ${contenedor} -- dnf -y install openssh-server
-        incus exec ${contenedor} -- systemctl start sshd
+        $my_manager exec ${contenedor} -- dnf -y update
+        $my_manager exec ${contenedor} -- dnf -y install $packages_to_install
+        $my_manager exec ${contenedor} -- dnf -y install openssh-server
+        $my_manager exec ${contenedor} -- systemctl start sshd
     ;;
     debian| ubuntu)
         if [ -n "$http_proxy" ]
         then
             proxyfile=$(mktemp)
             echo Acquire::http::Proxy \"${http_proxy}\"\; > $proxyfile
-            incus file push $proxyfile ${contenedor}/etc/apt/apt.conf.d/proxy.conf
+            $my_manager file push $proxyfile ${contenedor}/etc/apt/apt.conf.d/proxy.conf
             rm $proxyfile
         fi
-        incus exec ${contenedor} -- apt-get -y update
-        incus exec ${contenedor} -- apt-get -y upgrade
-        incus exec ${contenedor} -- apt-get -y install $packages_to_install
-        incus exec ${contenedor} -- apt-get -y install $packages_to_install_too
-        incus exec ${contenedor} -- apt-get -y install openssh-server
+        $my_manager exec ${contenedor} -- apt-get -y update
+        $my_manager exec ${contenedor} -- apt-get -y upgrade
+        $my_manager exec ${contenedor} -- apt-get -y install $packages_to_install
+        $my_manager exec ${contenedor} -- apt-get -y install $packages_to_install_too
+        $my_manager exec ${contenedor} -- apt-get -y install openssh-server
     ;;
 esac
 
 # Create & setup user
 case $ID in
     debian | centos)
-        incus file push add-user-miguel.sh ${contenedor}/root/add-user-miguel.sh
-        incus exec ${contenedor} -- /root/add-user-miguel.sh $UID
+        $my_manager file push add-user-miguel.sh ${contenedor}/root/add-user-miguel.sh
+        $my_manager exec ${contenedor} -- /root/add-user-miguel.sh $UID
 
         setup_my_user $UID /home/miguel
     ;;
